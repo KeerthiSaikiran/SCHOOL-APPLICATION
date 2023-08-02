81259f20a4a9145d6b6cc65fef1b32d260b5e2fd
Saikiran <Sai123@gmail.com>
2023-08-02 16:34:41 +0530

    New Commit

--- finance/views.py
+++ finance/views.py
@@ -9,8 +9,10 @@ def fee_collection(request):
 
 
 def fee_due_report(request):
-    return HttpResponse("This is a fee due report view")
+    # return HttpResponse("This is a fee due report view")
+    return render(request, 'finance/Fee Due Report.html')
 
 
 def fee_collection_report(request):
-    return HttpResponse("This is a fee collection report")
+    # return HttpResponse("This is a fee collection report")
+    return render(request, 'finance/fee-collection-report.html')
