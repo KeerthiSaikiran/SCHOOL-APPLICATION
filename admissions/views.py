c00404456ce344ab8964760003c7aa7b58c72a9f
Saikiran <Sai123@gmail.com>
2023-08-04 19:34:28 +0530

    New Commit

--- admissions/views.py
+++ admissions/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+
+# function based views
+
+def homepage(request):
+    return render(request, 'index.html')
+
 
 def admission(request):
     # return HttpResponse("This is admission view")
-    return render(request, 'admissions/add-admission.html')
+    return render(request, 'admissions/add-admission.html', {'name': 'Saikiran', 'greeting': 'Welcome'})
+
 
 def admission_report(request):
     # return HttpResponse("<h1>This is admission report view.</h1>")
